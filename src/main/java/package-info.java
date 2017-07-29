991e33dce16912bd7b37bfc4eb75fcbd53cf83b2
Denis Popov <d.popov93@mail.ru>
2017-07-30 00:50:27 +0300

    Added package-info

---
+++ src/main/java/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for MySQL working examples.
+ * I'm planning add some popular actions with database through java
+ *
+ * @author Denis Popov (mailto:d.popov93@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
---
+++ src/test/java/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package for test developed classes and methods
+ *
+ * @author Denis Popov (mailto:d.popov93@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
